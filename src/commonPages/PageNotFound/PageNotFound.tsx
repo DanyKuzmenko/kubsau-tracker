21b834d9fce41416934ff8dc3cec315a50eb1d4e
Mark <marknoviko@mail.ru>
2023-09-24 15:50:14 +0300

    error navigation refactor

--- src/commonPages/PageNotFound/PageNotFound.tsx
+++ src/commonPages/PageNotFound/PageNotFound.tsx
@@ -1,22 +1,22 @@
 import { useLocation, useNavigate } from 'react-router-dom';
-import {FC} from "react";
+import { FC } from 'react';
 
 type State = {
   state: {
-    prevPageLink?: string
-  }
-}
+    prevPageLink?: string;
+  };
+};
 
 const PageNotFound: FC = () => {
-  const navigate = useNavigate()
-  const { state }: State = useLocation()
+  const navigate = useNavigate();
+  const { state }: State = useLocation();
 
-  let previousPageNavigateAction
+  let previousPageNavigateAction;
 
   if (state?.prevPageLink) {
-    previousPageNavigateAction = () => navigate(state.prevPageLink!)
+    previousPageNavigateAction = () => navigate(-1);
   } else {
-    previousPageNavigateAction = () => navigate(-1)
+    previousPageNavigateAction = () => navigate('..');
   }
 
   return (
@@ -24,6 +24,6 @@ const PageNotFound: FC = () => {
       <h1>Данная страница не найдена</h1>
       <button onClick={previousPageNavigateAction}>Вернуться назад</button>
     </section>
-  )
-}
+  );
+};
 export default PageNotFound;
